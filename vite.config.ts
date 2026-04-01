7e62c09d60e17c9b00b1b034ba87f4bf8b1520d3
earnwithalee <earnwithalee@gmail.com>
2026-04-02 01:18:27 +0500

    Set explicit ngrok host in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    allowedHosts: 'all',
+    allowedHosts: ["bullous-kandis-tardiest.ngrok-free.dev"],
+    host: true,
   }
 })
