4ddb1776efb2d8e84b0fb5da6145c55b88315d7b
earnwithalee <earnwithalee@gmail.com>
2026-04-10 17:07:51 +0500

    feat: centralize application constants and configuration

---
+++ src/constants/index.ts
@@ -0,0 +1,16 @@
+import { celo, celoAlfajores } from 'wagmi/chains';
+
+export const CONTRACT_ADDRESS = '0xE7B16C2E34Fc3a347e3243FBEb3518830AfE647b';
+
+export const SUPPORTED_CHAINS = [celo, celoAlfajores];
+
+export const SOCIAL_LINKS = {
+  TWITTER: 'https://twitter.com/MiniGigApp',
+  WEBSITE: 'https://minigig.celo',
+  TALENT_APP: 'https://talent.app',
+};
+
+export const REWARD_VALUES = {
+  CHECK_IN: 10,
+  GIG_COMPLETION: 50,
+};
