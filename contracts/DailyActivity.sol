e33cc7c86efaaf075001ca7e04e2a9cdb7bd96c2
earnwithalee <earnwithalee@gmail.com>
2026-04-24 00:35:54 +0500

    security: integrate OpenZeppelin ReentrancyGuard for protected functions

--- contracts/DailyActivity.sol
+++ contracts/DailyActivity.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
 /**
  * @title DailyActivity
  * @dev A simple contract for recording daily user interactions on Celo.
  */
-contract DailyActivity {
+contract DailyActivity is ReentrancyGuard {
     mapping(address => uint256) public lastActivity;
     mapping(address => uint256) public activityCount;
     
