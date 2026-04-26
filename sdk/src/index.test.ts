05197901628c7828533084f5a1b29bac92add2ed
earnwithalee <earnwithalee@gmail.com>
2026-04-26 21:14:29 +0500

    test: add unit tests for MiniGigSDK initialization

--- sdk/src/index.test.ts
+++ sdk/src/index.test.ts
@@ -6,4 +6,15 @@ describe('MiniGigSDK', () => {
     const sdk = new MiniGigSDK();
     expect(sdk).toBeDefined();
   });
+
+  it('should have correct default RPC URL', () => {
+    const sdk = new MiniGigSDK();
+    expect(sdk['client'].transport.url).toBe('https://forno.celo.org');
+  });
+
+  it('should allow custom RPC URL', () => {
+    const customRpc = 'https://alfajores-forno.celo-testnet.org';
+    const sdk = new MiniGigSDK(customRpc);
+    expect(sdk['client'].transport.url).toBe(customRpc);
+  });
 });
